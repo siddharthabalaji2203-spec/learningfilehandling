398b67d971e5593cf03e7592f104f9aad391ae2c
Spectrax <siddharthabalaji2203@gmail.com>
2026-07-04 13:12:16 +0530

    updated journal.py

--- journal.py
+++ journal.py
@@ -1,6 +1,6 @@
-greeting  = "Hey! , What happened today ? "
-print( greeting )
-response = input()
-file = open("journal.txt","a")
-file.write("---------" + "\n" + response + "\n")
-file.close()
+    greeting  = "Hey! , What happened today ? "
+    print( greeting )
+    response = input()
+    file = open("journal.txt","a")
+    file.write("---------" + "\n" + response + "\n")
+    file.close()
