6976d7f0feb72712a67d2c759dafe3fdf851ab6a
Spectrax <siddharthabalaji2203@gmail.com>
2026-07-04 15:36:53 +0530

    mini calculator addition version

---
+++ calculator.py
@@ -0,0 +1,7 @@
+try:
+    num_1 = int(input("Enter your first number:"))
+    num_2 = int(input("Enter your next number:"))
+    addition = num_1 + num_2
+    print(addition)
+except:
+    print("Invalid input. Please enter numbers only.")
